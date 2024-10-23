569575576eff509df6551bc4f06ca2959eff39aa
unknown <shiluxyj@gmail.com>
2024-10-23 20:32:01 +0800

    update html attribute lang

--- src/components/LanguageGroup.vue
+++ src/components/LanguageGroup.vue
@@ -2,7 +2,7 @@
   <button
     v-for="l in languages"
     :key="l.code"
-    @click="toggleLanguage(l.code)"
+    @click="toggleLanguage(l.code, l.lang)"
     :class="{ 'text-blue-700 font-bold': curLanguage === l.code }"
     class="mx-1 hover:text-blue-500"
   >
@@ -24,12 +24,13 @@ const { locale } = useI18n()
 let curLanguage = ref(locale.value)
 
 let languages = reactive([
-  { name: 'English', code: 'en-US' },
-  { name: '中文', code: 'zh-TW' }
+  { name: 'English', code: 'en-US', lang: 'en' },
+  { name: '中文', code: 'zh-TW', lang: 'zh-TW' }
 ])
 
-const toggleLanguage = (code: string) => {
+const toggleLanguage = (code: string, lang: string) => {
   locale.value = code
   curLanguage.value = locale.value
+  document.documentElement.lang = lang
 }
 </script>
